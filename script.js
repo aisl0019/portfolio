52d2abdd00d1884c4321083c6a1f2f46d91bdd51
Aisling Kathrine <Aisling@Aislings-MacBook-Air.local>
2019-01-07 12:11:39 +0100

    finished version

---
+++ script.js
@@ -0,0 +1,19 @@
+// BACK TO TOP BUTTON
+//LÅNT FRA W3SCHOOLS: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
